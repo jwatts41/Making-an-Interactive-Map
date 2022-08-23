35eb65f8957cc5877a89df71f238d917655fab47
jwatts41 <wattsjonovan@yahoo.com>
2022-08-22 20:59:56 -0400

    added in user location and ping

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 // creation of map
-const myMap = L.map('map', {
-    center: [48.868672, 2.342130], 
-    zoom: 12,
-})
+const myMap = {
+    coordinates: [],
+    businesses: [],
+    map: {},
+    markers: {},
 
-// add openstreetmap tiles
+    buildMap() {
+        this.map = L.map('map', {
+            center: this.coordinates,
+            zoom: 10,
+        });
+//openstreetmap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: '15',
-}).addTo(myMap)
+}).addTo(this.map)
+// geo markers
+const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+
+    }
+}
+
+
+//get the user's location
+async function getCoords(){
+	const pos = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [pos.coords.latitude, pos.coords.longitude]
+}
+
+// on load
+window.onload = async () => {
+    const coords = await getCoords()
+    console.log(coords)
+    myMap.coordinates = coords
+    myMap.buildMap()
+}
